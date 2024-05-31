2207413a9257cea2e324be3dec03a68642222ef0
Hamza Hamud <53880692+hhamud@users.noreply.github.com>
2024-05-31 18:15:02 +0100

    feat: attempt to alter partial

--- crates/node/src/alephBFT.rs
+++ crates/node/src/alephBFT.rs
@@ -2,25 +2,28 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use finality_aleph::{
-    run_validator_node, AlephBlockImport, AlephConfig, AllBlockMetrics, BlockImporter, ChannelProvider, Justification,
-    JustificationTranslator, MillisecsPerBlock, Protocol, ProtocolNaming, RateLimiterConfig, RedirectingBlockImport,
-    SessionPeriod, SubstrateChainStatus, SubstrateNetwork, SyncOracle, TracingBlockImport, ValidatorAddressCache,
+    get_aleph_block_import, run_validator_node, AlephBlockImport, AlephConfig, AllBlockMetrics, BlockImporter,
+    ChannelProvider, FavouriteSelectChainProvider, Justification, JustificationTranslator, MillisecsPerBlock, Protocol,
+    ProtocolNaming, RateLimiterConfig, RedirectingBlockImport, SessionPeriod, SubstrateChainStatus, SubstrateNetwork,
+    SyncOracle, TracingBlockImport, ValidatorAddressCache,
 };
-use sc_service::error::Error as ServiceError;
-use madara_runtime::Block;
+use madara_runtime::{Block, RuntimeApi};
 use sc_consensus_aura::{ImportQueueParams, SlotProportion, StartAuraParams};
-use sc_service::{Configuration, TaskManager};
-use sc_telemetry::Telemetry;
+use sc_executor::NativeElseWasmExecutor;
+use sc_service::error::Error as ServiceError;
+use sc_service::{new_db_backend, Configuration, TaskManager};
+use sc_telemetry::{Telemetry, TelemetryWorker};
+use sp_api::ConstructRuntimeApi;
+use sp_consensus_aura::sr25519::AuthorityPair;
 
-use crate::import_queue::BlockImportPipeline;
-use crate::service::{BasicImportQueue, FullClient, FullSelectChain, FullBackend};
-use crate::starknet::MadaraBackend;
-
+use crate::genesis_block::MadaraGenesisBlockBuilder;
+use crate::import_queue::{build_manual_seal_queue_pipeline, BlockImportPipeline};
+use crate::service::{BasicImportQueue, ExecutorDispatch, FullBackend, FullClient, FullSelectChain};
+use crate::starknet::{db_config_dir, MadaraBackend};
 
 /// Contains the service components for the Aleph Consensus Mechanism
 pub struct AlephBFT {
-
-    ///AlephBFT unit creation delay (in ms)
+    /// AlephBFT unit creation delay (in ms)
     unit_creation_delay: u64,
 
     /// The addresses at which the node will be externally reachable for validator network
@@ -77,8 +80,6 @@ impl AlephBFT {
     }
 }
 
-
-
 type FullPool = sc_transaction_pool::FullPool<Block, FullClient>;
 type Service = sc_service::PartialComponents<
     FullClient,
@@ -89,9 +90,118 @@ type Service = sc_service::PartialComponents<
     (Arc<MadaraBackend>, BlockImportPipeline, Option<Telemetry>),
 >;
 
+pub fn new_aleph_partial(config: &Configuration) -> Result<Service, ServiceError>
+where
+    RuntimeApi: ConstructRuntimeApi<Block, FullClient>,
+    RuntimeApi: Send + Sync + 'static,
+{
+    let telemetry = config
+        .telemetry_endpoints
+        .clone()
+        .filter(|x| !x.is_empty())
+        .map(|endpoints| -> Result<_, sc_telemetry::Error> {
+            let worker = TelemetryWorker::new(16)?;
+            let telemetry = worker.handle().new_telemetry(endpoints);
+            Ok((worker, telemetry))
+        })
+        .transpose()?;
 
-pub fn new_aleph_partial(config: &Configuration) -> Result<Service, ServiceError> {
-    todo!()
+    let executor = sc_service::new_native_or_wasm_executor(config);
+
+    let backend = new_db_backend(config.db_config())?;
+
+    let genesis_block_builder = MadaraGenesisBlockBuilder::<Block, _, _>::new(
+        config.chain_spec.as_storage_builder(),
+        true,
+        backend.clone(),
+        executor.clone(),
+    )
+    .unwrap();
+
+    let (client, backend, keystore_container, task_manager) = sc_service::new_full_parts_with_genesis_builder::<
+        Block,
+        RuntimeApi,
+        _,
+        MadaraGenesisBlockBuilder<Block, FullBackend, NativeElseWasmExecutor<ExecutorDispatch>>,
+    >(
+        config,
+        telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
+        executor,
+        backend,
+        genesis_block_builder,
+    )?;
+
+    let client = Arc::new(client);
+
+    let telemetry = telemetry.map(|(worker, telemetry)| {
+        task_manager.spawn_handle().spawn("telemetry", None, worker.run());
+        telemetry
+    });
+
+    let select_chain = sc_consensus::LongestChain::new(backend.clone());
+
+    let transaction_pool = sc_transaction_pool::BasicPool::new_full(
+        config.transaction_pool.clone(),
+        config.role.is_authority().into(),
+        config.prometheus_registry(),
+        task_manager.spawn_essential_handle(),
+        client.clone(),
+    );
+
+    let madara_backend = Arc::new(MadaraBackend::open(&config.database, &db_config_dir(config))?);
+
+    let justification_translator = JustificationTranslator::new(
+        SubstrateChainStatus::new(backend.clone())
+            .map_err(|e| ServiceError::Other(format!("failed to set up chain status: {e}")))?,
+    );
+    let metrics = AllBlockMetrics::new(config.prometheus_registry());
+    let justification_channel_provider = ChannelProvider::new();
+    let select_chain_provider = FavouriteSelectChainProvider::default();
+
+    let aleph_block_import = get_aleph_block_import(
+        client.clone(),
+        justification_channel_provider.get_sender(),
+        justification_translator,
+        select_chain_provider.select_chain(),
+        metrics.clone(),
+    );
+
+    let slot_duration = sc_consensus_aura::slot_duration(&*client)?;
+
+    // DO NOT change Aura parameters without updating the finality-aleph sync accordingly,
+    // in particular the code responsible for verifying incoming Headers, as it is supposed
+    // to duplicate parts of Aura internal logic
+    let import_queue = sc_consensus_aura::import_queue::<AuthorityPair, _, _, _, _, _>(ImportQueueParams {
+        block_import: aleph_block_import.clone(),
+        justification_import: Some(Box::new(aleph_block_import)),
+        client: client.clone(),
+        create_inherent_data_providers: move |_, ()| async move {
+            let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+
+            let slot = sp_consensus_aura::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
+                *timestamp,
+                slot_duration,
+            );
+
+            Ok((slot, timestamp))
+        },
+        spawner: &task_manager.spawn_essential_handle(),
+        registry: config.prometheus_registry(),
+        check_for_equivocation: Default::default(),
+        telemetry: telemetry.as_ref().map(|x| x.handle()),
+        compatibility_mode: Default::default(),
+    })?;
+
+    Ok(sc_service::PartialComponents {
+        client,
+        backend,
+        task_manager,
+        import_queue,
+        keystore_container,
+        select_chain,
+        transaction_pool,
+        other: (madara_backend, None, telemetry),
+    })
 }
 
 pub fn new_aleph_full(config: &Configuration) -> Result<TaskManager, ServiceError> {
